f1752514747fe7810cf478d0625a648daf0a193f
亜優 公田 <k024g0061@g.neec.ac.jp>
2025-02-07 16:14:29 +0900

    プロジェクト ファイルを追加します。

---
+++ Object.h
@@ -0,0 +1,16 @@
+#pragma once
+#include"Structer.h"
+
+class Object
+{
+protected:
+
+	Vector2 pos_;
+	Vector2 velocity_;
+	bool isActive_;
+
+public:
+	Object();
+	~Object();
+
+};
---
+++ Structer.h
@@ -0,0 +1,9 @@
+#pragma once
+
+struct Vector2
+{
+	float x;
+	float y;
+};
+
+
